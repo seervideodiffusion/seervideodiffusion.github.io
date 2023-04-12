fb740831936c184c3980457c50d3e27b57563737
Xianfan Gu <529632102@qq.com>
2023-04-12 10:36:24 +0800

    new page version1

---
+++ sm/assets/teaser-data.js
@@ -0,0 +1,25 @@
+file_paths = {
+    "move_pen_down": "./sm/assets/our_results/sthv2/image10_ours_2.GIF",
+    "move_pen_up": "./sm/assets/our_results/sthv2/image10_ours_1.GIF",
+
+    "turn_fan_left": "./sm/assets/our_results/sthv2/image11_ours_1.GIF",
+    "turn_fan_right": "./sm/assets/our_results/sthv2/image11_ours_2.GIF",
+
+    "remove_scissor": "./sm/assets/our_results/sthv2/image12_ours_1.GIF",
+    "pickup_scissor": "./sm/assets/our_results/sthv2/image12_ours_2.GIF",
+
+    "pretend_pick_juice": "./sm/assets/our_results/sthv2/image13_ours_2.GIF",
+    "pick_up_juice": "./sm/assets/our_results/sthv2/image13_ours_1.GIF",
+
+    "put_banana_plate": "./sm/assets/our_results/sthv2/image14_ours_1.GIF",
+    "put_corn_plate": "./sm/assets/our_results/sthv2/image14_ours_2.GIF",
+
+    "pick_green_cup": "./sm/assets/our_results/sthv2/image15_ours_1.GIF",
+    "pick_glass_cup": "./sm/assets/our_results/sthv2/image15_ours_2.GIF",
+
+    "turn_lever_front": "./sm/assets/our_results/sthv2/image16_ours_1.GIF",
+    "pick_up_knife": "./sm/assets/our_results/sthv2/image16_ours_2.GIF",
+
+    "pick_up_bowl": "./sm/assets/our_results/sthv2/image17_ours_1.GIF",
+    "close_box_flap": "./sm/assets/our_results/sthv2/image17_ours_2.GIF",
+}
